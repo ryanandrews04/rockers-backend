be234eec80efda91b482a09c0610a66ca92b6c76
Ryan Andrews <rcandrews44@gmail.com>
2021-07-07 02:51:48 -0400

    completed adding posts

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -6,5 +6,22 @@ class Api::V1::PostsController < ApplicationController
         posts = Post.all
         render json: posts
     end
-    
+
+    def create
+        post = Post.new(posts_params)
+        if post.valid?
+            post.save
+            render json: post
+        else 
+            render json: {errors: post.errors.full_messages}, status: :unprocessable_entity
+        end
+        
+    end
+
+    private
+
+    def posts_params
+        params.permit(:user_id, :title, :text, :image)
+    end
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ApplicationRecord
     has_many :comments
     belongs_to :user
+
+    validates :title, presence: true
+    validates :text, presence: true
 end
